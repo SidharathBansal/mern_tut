f93000e5630d548c1358e522d2e85a496afbae8b
Sidbansal21 <sidharath0974.cse19@gmail.com>
2022-05-04 11:09:00 +0530

    database setup

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -49,16 +49,14 @@ const updateGoal = asyncHandler(async (req, res) => {
 // @route DELETE /api/goals/:id
 // @access Private
 const deleteGoal = asyncHandler(async (req, res) => {
-  const goal = await Goal.findById(req.params.id)
+  const goal = await Goal.findByIdAndRemove(req.params.id);
 
   if(!goal){
     res.status(400)
     throw new Error('Goal not found')
   }
 
-  const deletedGoal = await goal.remove();
-
-  res.status(200).json(deletedGoal);
+  res.status(200).json(goal);
 });
 
 module.exports = {
