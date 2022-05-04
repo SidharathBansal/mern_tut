ddb8d038b0c6a410af80ebbce8e6b24382c0c608
unknown <sushil.saffrontech@gmail.com>
2022-05-04 19:17:56 +0530

    protected routes

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -6,7 +6,8 @@ const {
   updateGoal,
   deleteGoal,
 } = require("../controllers/goalController");
-const {protect} = require('../middleware/authMiddleware')
+const { protect } = require("../middleware/authMiddleware");
+
 router.route("/").get(protect, getGoals).post(protect, setGoal);
 router.route("/:id").put(protect, updateGoal).delete(protect, deleteGoal);
 
